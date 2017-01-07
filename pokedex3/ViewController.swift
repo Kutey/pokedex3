2ce584c90c6e0d351b53bd8982ec09acf3b3239a
Yi Zhang <zhangyi19920209@gmail.com>
2017-01-07 12:42:24 -0500

    awesome commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Yi Zhang on 1/6/17.
+//  Copyright © 2017 Yi Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
